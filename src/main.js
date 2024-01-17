6b5f233fb7147ea6d7c0a73739392fcd7957c528
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2024-01-16 21:24:56 -0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import './app.css';
+
+document.querySelector('#app').innerHTML = `
+
+<div class="hero min-h-screen bg-base-200">
+
+  
+ <iframe id="iframe" title="Vegas Vickie 1980" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/a3ff10a1c4bf45ffb6523d2e13ea44fd/embed?autospin=1&autostart=1&ui_theme=dark"> </iframe> 
+ 
+ 
+
+    
+  
+  </div>
+
+
+
+
+`;
